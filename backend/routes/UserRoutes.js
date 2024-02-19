2646205b38c513c5148262f15d8641a03b811dea
Renanmdr <renanmdr3@gmail.com>
2024-02-19 16:19:45 -0300

    criado rotas e controller login, checkUser, getUserById, editUser

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,14 @@ import { Router } from 'express'
 const router = Router()
 import { UserController } from '../controllers/UserController.js'
 
+// middleware to validate token
+import { verifyToken } from '../helpers/verify-token.js'
+
 router.post('/register', UserController.resgister)
+router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
+router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 
 
